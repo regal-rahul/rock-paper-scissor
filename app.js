1231b32a62e4f339c5dd3457384874758c06baaf
regal-rahul <regalrahul777@gmail.com>
2022-07-08 11:39:05 +0530

    RPS logics completed

--- app.js
+++ app.js
@@ -1,12 +1,87 @@
-const move = ['rock', 'raper', 'scissor'];
+function computerPlay() {
+    let random = Math.random();
+    if (random <= 0.3333) {
+        return "paper";
+    } else if (random >= 0.6666) {
+        return "rock";
+    } else {
+        return "scissors";
+    }
+}
 
 
-function computerMove() {
-    var number = Math.floor(Math.random()*3);
-    var movePlayed = move[number];
-    return movePlayed.toUpperCase();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "paper") {
+            computerScore++;
+            return lose;
+        } else if (computerSelection === "rock") {
+            return tie;
+        } else {
+            userScore++;
+            return win;
+        }
+    }
+
+    if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "paper") {
+            userScore++;
+            return win;
+        } else if (computerSelection === "rock") {
+            computerScore++;
+            return lose;
+        } else {
+            return tie;
+        }
+    }
+
+    if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "paper") {
+            return tie;
+        } else if (computerSelection === "rock") {
+            userScore++;
+            return win;
+        } else {
+            computerScore++;
+            return lose;
+        }
+    }
 }
-function playerMove() {
-    var playerinput = prompt("Enter your Move");
-    return playerinput.toUpperCase();
+
+
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
+let win = "You win"
+let lose = "You lose"
+let tie = "It is a tie"
+
+var i = 0;
+const play = () => {
+    let playerSelection = prompt("Pick a move");
+    const computerSelection = computerPlay()
+    console.log(playRound(playerSelection, computerSelection));
+    i++;
+    if (i !== 5) {
+        play();
+    } else {
+        declareWinner();
+        alert("Game Over");
+    }
 }
+const declareWinner = () => {
+    if (i === 5 && userScore === computerScore) {
+        console.log("Your score = " + userScore);
+        console.log("Computer's score = " + computerScore);
+        console.log("It was a Draw, Try Again!");
+    } else if (i === 5 && userScore > computerScore) {
+        console.log("Your score = " + userScore);
+        console.log("Computer's score = " + computerScore);
+        console.log("You won!");
+    } else {
+        console.log("Your score = " + userScore);
+        console.log("Computer's score = " + computerScore);
+        console.log("Computer won!");
+    }
+}
+
+play();
